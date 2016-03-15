4be44e7948cf8a9790bcde4c071c8a250fa6c95d
DavidMimnagh <davidmimnagh1@googlemail.com>
2016-03-15 17:38:27 +0000

    Almost done!

--- Nbodies/nbody.cu
+++ Nbodies/nbody.cu
@@ -31,7 +31,7 @@
 #include <curand_kernel.h>
 #include <algorithm>
 
-#define GPUTEST 1
+#define GPUTEST 0
 
 
 using type = float;
@@ -54,12 +54,56 @@ struct planet {
 	T mass;
 };
 
+struct planet<type> golden_bodies[5] = {
+	{                               /* sun */
+		0, 0, 0, 0, 0, 0, solar_mass
+	},
+	{                               /* jupiter */
+		4.84143144246472090e+00,
+		-1.16032004402742839e+00,
+		-1.03622044471123109e-01,
+		1.66007664274403694e-03 * days_per_year,
+		7.69901118419740425e-03 * days_per_year,
+		-6.90460016972063023e-05 * days_per_year,
+		9.54791938424326609e-04 * solar_mass
+	},
+	{                               /* saturn */
+		8.34336671824457987e+00,
+		4.12479856412430479e+00,
+		-4.03523417114321381e-01,
+		-2.76742510726862411e-03 * days_per_year,
+		4.99852801234917238e-03 * days_per_year,
+		2.30417297573763929e-05 * days_per_year,
+		2.85885980666130812e-04 * solar_mass
+	},
+	{                               /* uranus */
+		1.28943695621391310e+01,
+		-1.51111514016986312e+01,
+		-2.23307578892655734e-01,
+		2.96460137564761618e-03 * days_per_year,
+		2.37847173959480950e-03 * days_per_year,
+		-2.96589568540237556e-05 * days_per_year,
+		4.36624404335156298e-05 * solar_mass
+	},
+	{                               /* neptune */
+		1.53796971148509165e+01,
+		-2.59193146099879641e+01,
+		1.79258772950371181e-01,
+		2.68067772490389322e-03 * days_per_year,
+		1.62824170038242295e-03 * days_per_year,
+		-9.51592254519715870e-05 * days_per_year,
+		5.15138902046611451e-05 * solar_mass
+	}
+};
+
+const type DT{ 1e-2 };
+const type RECIP_DT{ static_cast<type>(1.0) / DT };
+
 
 //velocity update for the kernals
 template <typename T>
-__global__ void adv_Velocity_Update(int nbodies, planet<T> *bodies)
+__global__ void adv_Update_GPU(int nbodies, planet<T> *bodies)
 {
-
 	int i = threadIdx.x + blockIdx.x*blockDim.x;
 
 	if (i < nbodies)
@@ -82,26 +126,12 @@ __global__ void adv_Velocity_Update(int nbodies, planet<T> *bodies)
 			b2.vz += dz * b1.mass  * mag;
 		}
 
-		planet<T> &b = bodies[i];
-		b.x += b.vx;
-		b.y += b.vy;
-		b.z += b.vz;
+		b1.x += b1.vx;
+		b1.y += b1.vy;
+		b1.z += b1.vz;
 	}
-}
 
-//position update for the kernals
-template <typename T>
-__global__ void adv_Position_Update(int nbodies, planet<T> *bodies)
-{
-	int i = threadIdx.x + blockIdx.x*blockDim.x;
-
-	if (i < nbodies)
-	{
-		planet<T> &b = bodies[i];
-		b.x += b.vx;
-		b.y += b.vy;
-		b.z += b.vz;
-	}
+	
 }
 
 template <typename T>
@@ -118,71 +148,71 @@ __global__ void scale_bodies_GPU(int nbodies, planet<T> *bodies, T scale)
 	}
 }
 
-template <typename T>
-__global__ void energy_GPU(int nbodies, T *addReduc, T *subReduc, planet<T> *bodies)
-{
-	extern __shared__ T e[];
-
-	//T e = 0.0;
-	unsigned int threadID = threadIdx.x;
-
-	unsigned int i = threadIdx.x + blockIdx.x*blockDim.x;
-
-
-
-	if (i < nbodies)
-	{
-		planet<T> &b = bodies[i];
-		e[threadID] = 0.5 * b.mass * (b.vx * b.vx + b.vy * b.vy + b.vz * b.vz);
-	}
-	__syncthreads();
-
-	for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1)
-	{
-		if (threadID < stride)
-		{
-			e[threadID] += e[threadID + stride];
-		}
-		__syncthreads();
-	}
-	if (threadID == 0)
-	{
-		addReduc[blockIdx.x] = e[0];
-	}
-
-	__syncthreads();
-	e[threadID] = 0;
-
-	if (i < nbodies)
-	{
-		for (int iter = i + 1; iter < nbodies; iter++){
-			planet<T> &b = bodies[i];
-			planet<T> &b2 = bodies[iter];
-			T dx = b.x - b2.x;
-			T dy = b.y - b2.y;
-			T dz = b.z - b2.z;
-			T distance = sqrt(dx * dx + dy * dy + dz * dz);
-			T var = ((b.mass * b2.mass) / distance);
-			e[threadID] += var;
-		}
-	}
-
-	__syncthreads();
-
-	for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1)
-	{
-		if (threadID < stride)
-		{
-			e[threadID] += e[threadID + stride];
-		}
-		__syncthreads();
-	}
-
-	if (threadID == 0)
-	{
-		subReduc[blockIdx.x] = e[0];
-	}
-}
+//template <typename T>
+//__global__ void energy_GPU(int nbodies, T *addReduc, T *subReduc, planet<T> *bodies)
+//{
+//	extern __shared__ T e[];
+//
+//	//T e = 0.0;
+//	unsigned int threadID = threadIdx.x;
+//
+//	unsigned int i = threadIdx.x + blockIdx.x*blockDim.x;
+//
+//
+//
+//	if (i < nbodies)
+//	{
+//		planet<T> &b = bodies[i];
+//		e[threadID] = 0.5 * b.mass * (b.vx * b.vx + b.vy * b.vy + b.vz * b.vz);
+//	}
+//
+//
+//	for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1)
+//	{
+//		if (threadID < stride)
+//		{
+//			e[threadID] += e[threadID + stride];
+//		}
+//
+//	}
+//	if (threadID == 0)
+//	{
+//		addReduc[blockIdx.x] = e[0];
+//	}
+//
+//
+//	e[threadID] = 0;
+//
+//	if (i < nbodies)
+//	{
+//		for (int iter = i + 1; iter < nbodies; iter++){
+//			planet<T> &b = bodies[i];
+//			planet<T> &b2 = bodies[iter];
+//			T dx = b.x - b2.x;
+//			T dy = b.y - b2.y;
+//			T dz = b.z - b2.z;
+//			T distance = sqrt(dx * dx + dy * dy + dz * dz);
+//			T var = ((b.mass * b2.mass) / distance);
+//			e[threadID] += var;
+//		}
+//	}
+//
+//	__syncthreads();
+//
+//	for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1)
+//	{
+//		if (threadID < stride)
+//		{
+//			e[threadID] += e[threadID + stride];
+//		}
+//		__syncthreads();
+//	}
+//
+//	if (threadID == 0)
+//	{
+//		subReduc[blockIdx.x] = e[0];
+//	}
+//}
 
 template <typename T>
 __global__ void offset_momentum_GPU(planet<T> *bodies, T *outData, int nbodies, int d_gridSize, T solarMass)
@@ -190,27 +220,23 @@ __global__ void offset_momentum_GPU(planet<T> *bodies, T *outData, int nbodies, 
 
 	T px = 0.0, py = 0.0, pz = 0.0;
 	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+	unsigned int doCount = 0;
+	reduction_offset_mom(nbodies, bodies, outData, 0);
 
-	reduction_offset_mom(bodies, outData, 0, nbodies);
-	if (i > 0 && i < d_gridSize){
-		outData[0] += outData[i];
+	if (doCount < 3)
+	{
+		if (i > 0 && i < d_gridSize)
+		{
+			outData[0] += outData[i];
+		}
+		if (doCount == 0)
+		px = outData[0];
+		else if (doCount == 1)
+			py = outData[0];
+		else if (doCount == 2)
+			pz = outData[0];
+		doCount++;
 	}
-	px = outData[0];
-	__syncthreads();
-
-	reduction_offset_mom(bodies, outData, 1, nbodies);
-	if (i > 0 && i < d_gridSize){
-		outData[0] += outData[i];
-	}
-	py = outData[0];
-	__syncthreads();
-
-	reduction_offset_mom(bodies, outData, 2, nbodies);
-	if (i > 0 && i < d_gridSize){
-		outData[0] += outData[i];
-	}
-	pz = outData[0];
-	__syncthreads();
 
 	bodies[0].vx = -px / solarMass;
 	bodies[0].vy = -py / solarMass;
@@ -218,7 +244,8 @@ __global__ void offset_momentum_GPU(planet<T> *bodies, T *outData, int nbodies, 
 }
 
 template <typename T>
-__device__ void reduction_offset_mom(planet<T> *bodies, T *outData, int step, int nbodies) {
+__device__ void reduction_offset_mom(int nbodies, planet<T> *bodies, T *outData, int step)
+{
 	extern __shared__ T sharedData[];
 	// each thread loads one element from global to shared mem
 	unsigned int threadID = threadIdx.x;
@@ -229,21 +256,16 @@ __device__ void reduction_offset_mom(planet<T> *bodies, T *outData, int step, in
 		if (i < nbodies)
 			sharedData[threadID] = bodies[i].vx * bodies[i].mass;
 
-		__syncthreads();
 	}
 	else if (step == 1)
 	{
 		if (i < nbodies)
 			sharedData[threadID] = bodies[i].vy * bodies[i].mass;
-
-		__syncthreads();
 	}
 	else if (step == 2)
 	{
 		if (i < nbodies)
 			sharedData[threadID] = bodies[i].vz * bodies[i].mass;
-
-		__syncthreads();
 	}
 
 
@@ -252,7 +274,7 @@ __device__ void reduction_offset_mom(planet<T> *bodies, T *outData, int step, in
 		if (threadID < s) {
 			sharedData[threadID] += sharedData[threadID + s];
 		}
-		__syncthreads();
+
 	}
 	// write result for this block to global mem
 	if (threadID == 0)
@@ -323,51 +345,6 @@ void offset_momentum(int nbodies, planet<T> *bodies)
 	bodies[0].vz = -pz / solar_mass;
 }
 
-struct planet<type> golden_bodies[5] = {
-		{                               /* sun */
-			0, 0, 0, 0, 0, 0, solar_mass
-		},
-		{                               /* jupiter */
-			4.84143144246472090e+00,
-			-1.16032004402742839e+00,
-			-1.03622044471123109e-01,
-			1.66007664274403694e-03 * days_per_year,
-			7.69901118419740425e-03 * days_per_year,
-			-6.90460016972063023e-05 * days_per_year,
-			9.54791938424326609e-04 * solar_mass
-		},
-		{                               /* saturn */
-			8.34336671824457987e+00,
-			4.12479856412430479e+00,
-			-4.03523417114321381e-01,
-			-2.76742510726862411e-03 * days_per_year,
-			4.99852801234917238e-03 * days_per_year,
-			2.30417297573763929e-05 * days_per_year,
-			2.85885980666130812e-04 * solar_mass
-		},
-		{                               /* uranus */
-			1.28943695621391310e+01,
-			-1.51111514016986312e+01,
-			-2.23307578892655734e-01,
-			2.96460137564761618e-03 * days_per_year,
-			2.37847173959480950e-03 * days_per_year,
-			-2.96589568540237556e-05 * days_per_year,
-			4.36624404335156298e-05 * solar_mass
-		},
-		{                               /* neptune */
-			1.53796971148509165e+01,
-			-2.59193146099879641e+01,
-			1.79258772950371181e-01,
-			2.68067772490389322e-03 * days_per_year,
-			1.62824170038242295e-03 * days_per_year,
-			-9.51592254519715870e-05 * days_per_year,
-			5.15138902046611451e-05 * solar_mass
-		}
-};
-
-const type DT{ 1e-2 };
-const type RECIP_DT{ static_cast<type>(1.0) / DT };
-
 /*
  * Rescale certain properties of bodies. That allows doing
  * consequential advance()'s as if dt were equal to 1.0.
@@ -416,29 +393,29 @@ void callOffSet(int nbodies, planet<T> *Gbodies)
 	cudaFree(d_Array);
 }
 
-template <typename T>
-T callEnergy(int nbodies, planet<T> *Gbodies)
-{
-	
-	T *h_addArray = new T[gridSize];
-	T *h_subArray = new T[gridSize];
-
-	T *d_addArray; cudaMalloc((void**)&d_addArray, gridSize * sizeof(T));
-	T *d_subArray; cudaMalloc((void**)&d_subArray, gridSize * sizeof(T));
-
-	energy_GPU << <gridSize, blockSize, nbodies * sizeof(T) >> >(nbodies, d_addArray, d_subArray, Gbodies);
-	cudaMemcpy(h_addArray, d_addArray, gridSize * sizeof(T), cudaMemcpyDeviceToHost);
-	cudaMemcpy(h_subArray, d_subArray, gridSize * sizeof(T), cudaMemcpyDeviceToHost);
-
-	for (int i = 1; i < gridSize; i++){
-		h_addArray[0] += h_addArray[i];
-		h_subArray[0] += h_subArray[i];
-	}
-
-	T e = h_addArray[0] - h_subArray[0];
-
-	return e;
-	}
+//template <typename T>
+//T callEnergy(int nbodies, planet<T> *Gbodies)
+//{
+//	
+//	T *h_addArray = new T[gridSize];
+//	T *h_subArray = new T[gridSize];
+//
+//	T *d_addArray; cudaMalloc((void**)&d_addArray, gridSize * sizeof(T));
+//	T *d_subArray; cudaMalloc((void**)&d_subArray, gridSize * sizeof(T));
+//
+//	energy_GPU << <gridSize, blockSize, nbodies * sizeof(T) >> >(nbodies, d_addArray, d_subArray, Gbodies);
+//	cudaMemcpy(h_addArray, d_addArray, gridSize * sizeof(T), cudaMemcpyDeviceToHost);
+//	cudaMemcpy(h_subArray, d_subArray, gridSize * sizeof(T), cudaMemcpyDeviceToHost);
+//
+//	for (int i = 1; i < gridSize; i++){
+//		h_addArray[0] += h_addArray[i];
+//		h_subArray[0] += h_subArray[i];
+//	}
+//
+//	T e = h_addArray[0] - h_subArray[0];
+//
+//	return e;
+//	}
 
 int main(int argc, char ** argv)
 {
@@ -469,7 +446,6 @@ int main(int argc, char ** argv)
 	outData = new type[gridSize];
 
 
-	//auto t1 = std::chrono::steady_clock::now();
 	if (!GPUTEST)
 	{
 		t1 = std::chrono::steady_clock::now();
@@ -489,62 +465,58 @@ int main(int argc, char ** argv)
 		cudaMalloc(&Gbodies, nbodies*sizeof(planet<type>));
 		cudaMemcpy(Gbodies, bodies, nbodies*sizeof(planet<type>), cudaMemcpyHostToDevice);
 
-		cudaOccupancyMaxPotentialBlockSize(&mingridSize, &blockSize, scale_bodies_GPU<type>, 0, nbodies);
-
-		gridSize = (nbodies + blockSize - 1) / blockSize;
-		if (!isPowerOfTwo(blockSize)){
-			blockSize = pow(2, ceil(log(blockSize) / log(2)));
-		}
-		if (blockSize > 1024){
-			int multiple = blockSize / 1024;
-			gridSize = multiple;
-		}
+		//cudaOccupancyMaxPotentialBlockSize(&mingridSize, &blockSize, scale_bodies_GPU<type>, 0, nbodies);
+		int maxThreads;
+		cudaDeviceGetAttribute(&maxThreads, cudaDevAttrMaxThreadsPerBlock, 0);
+		blockSize = maxThreads;
+		gridSize = (nbodies + blockSize) / blockSize;
+		
 
 		t1 = std::chrono::steady_clock::now();
 
-		//offset_momentum(nbodies, bodies);
 		callOffSet(nbodies, Gbodies);
 		cudaThreadSynchronize();
 
-		e1 = energy(nbodies, bodies); //callEnergy(nbodies, Gbodies);
+		cudaMemcpy(bodies, Gbodies, nbodies*sizeof(planet<type>), cudaMemcpyDeviceToHost);
+		e1 = energy(nbodies, bodies);
+		cudaMemcpy(Gbodies, bodies, nbodies*sizeof(planet<type>), cudaMemcpyHostToDevice);
 
-		//Scaling
-		scale_bodies_GPU << <gridSize, blockSize >> >(nbodies, Gbodies, DT);//scale_bodies(nbodies, bodies, DT);
+		//Scaling initial
+		scale_bodies_GPU << <gridSize, blockSize >> >(nbodies, Gbodies, DT);
 
 		Tadv = std::chrono::steady_clock::now();
 		for (auto i = 0; i < niters; ++i)
 		{
-			//Advaced
-			adv_Velocity_Update << <gridSize, blockSize >> >(nbodies, Gbodies);
-
-			//adv_Position_Update << <gridSize, blockSize >> >(nbodies, Gbodies);
+			//Calling advanced
+			adv_Update_GPU << <gridSize, blockSize >> >(nbodies, Gbodies);
+		//	adv_Position_Update << <gridSize, blockSize >> >(nbodies, Gbodies);
 		}
 		Tadv2 = std::chrono::steady_clock::now();
 
-		//Scaling2
+		//Scaling again
 		scale_bodies_GPU << <gridSize, blockSize >> >(nbodies, Gbodies, RECIP_DT);
 
-
-		e2 = energy(nbodies, bodies); //callEnergy(nbodies, Gbodies);
+		cudaMemcpy(bodies, Gbodies, nbodies*sizeof(planet<type>), cudaMemcpyDeviceToHost);
+		e2 = energy(nbodies, bodies);
 
 		t2 = std::chrono::steady_clock::now();
 
 		//copy data back to CPU
-		cudaMemcpy(bodies, Gbodies, nbodies*sizeof(planet<type>), cudaMemcpyDeviceToHost);
+		//cudaMemcpy(bodies, Gbodies, nbodies*sizeof(planet<type>), cudaMemcpyDeviceToHost);
 
 		//Free up the memory
 		cudaFree(Gbodies);
 	}
 
-	//auto t2 = std::chrono::steady_clock::now();
 	auto diff = t2 - t1;
 	auto diff2 = Tadv2 - Tadv;
 
 	std::cout << std::setprecision(9);
 	std::cout << e1 << '\n' << e2 << '\n';
-	//std::cout << std::fixed << std::setprecision(3);
 	std::cout << std::chrono::duration<double>(diff).count() << " seconds.\n";
 	std::cout <<"adv: " << std::chrono::duration<double>(diff2).count() << " seconds.\n";
+	std::cout << "GridSize: " << gridSize << std::endl;
+	
 	delete[]outData;
 	if (argc != 1) { delete[] bodies; }
 	return 0;
